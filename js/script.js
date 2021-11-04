e48c7f9c7c3012e55ce912bbcc9ffcc888af1c8a
Salvatore949 <salvo.mess49@gmail.com>
2021-11-04 15:42:31 +0100

    array di oggetti

---
+++ js/script.js
@@ -0,0 +1,47 @@
+// Creo un array di oggetti
+// creo un append per stampare in pagina 
+
+var classi = [
+
+    {   
+        "immagine": "img/wayne-barnett-founder-ceo.jpg",
+        "nome personaggio":"Wayne Bernett",
+        "ruolo":"Founder e CEO"
+    },
+
+    {
+        "immagine": "img/angela-caroll-chief-editor.jpg",
+        "nome personaggio":"Angela Carol",
+        "ruolo":"Chief Editor"
+    },
+
+    {
+        "immagine": "img/walter-gordon-office-manager.jpg",
+        "nome personaggio":"Walter Gordon",
+        "ruolo":"Officer Manager"
+    },
+
+    {
+        "immagine": "img/angela-lopez-social-media-manager.jpg",
+        "nome personaggio":"Angela Lopez",
+        "ruolo":"Social Media Manager"
+    },
+
+    {
+        "immagine": "img/scott-estrada-developer.jpg",
+        "nome personaggio":"Scott Estrada",
+        "ruolo":"Developer"
+    },
+
+    {
+        "immagine": "img/barbara-ramos-graphic-designer.jpg",
+        "nome personaggio":"Barbara Ramos",
+        "ruolo":"Graphic Designer"
+    }
+
+];
+
+console.log(classi);
+
+
+
